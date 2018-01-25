cdff75b804201613613c49dca68ae9d617181907
Firoz Kamdar <firozkamdar@gmail.com>
2018-01-25 09:46:41 -0800

    added apollo, boomer, helo, etc.

--- SLL.js
+++ SLL.js
@@ -1,5 +1,17 @@
+'use strict';
+
 const LinkedList = require('./classes.js')
 
+const SLL = new LinkedList();
+
 function main(){
-    
+  SLL.insertFirst('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('Starbuck');    
+  console.log(JSON.stringify(SLL));
+
 }
+
+main();
--- classes.js
+++ classes.js
@@ -9,9 +9,11 @@ class LinkedList {
     constructor() {
             this.head = null;
     }
+    
     insertFirst(item){
         this.head = new _Node(item, this.head);
     }
+
     insertLast(item){
         if(this.head === null){
             this.insertFirst(item);
